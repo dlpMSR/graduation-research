fc922e1c5a0e831dc2bbc925ddd59d77c90009e5
DESKTOP-NUB72QV\nagiclocal <pntsec_msr@outlook.com>
2018-01-29 22:03:38 +0900

    特徴量を追加 中心の棒追加

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 kigou = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def graph():
-    with open('./images/images_testing3/output/output1.json','r') as input:
+    with open('./images/images_testing2/output/output1.json','r') as input:
         json_dict = json.load(input)
 
         for item in json_dict:
@@ -20,7 +20,7 @@ def graph():
             
             for i in range(count):
                 name, ext = os.path.splitext(filename)
-                outputpass = os.path.join('./images/images_testing3/graph','%s.png' % name)
+                outputpass = os.path.join('./images/images_testing2/graph','%s.png' % name)
 
                 list_x = np.append(list_x, item[kigou[i]]["x"])
                 list_y = np.append(list_y, height - item[kigou[i]]["y"])
@@ -48,6 +48,14 @@ def graph():
                 d2_average = np.average(list_d2)
                 d2_average_sqrt = int(np.sqrt(d2_average))
 
+                #mean_x = np.mean(list_x)
+                min_x = min(list_x)
+                max_x = max(list_x)
+                center = int(min_x + (max_x - min_x)/2)
+                median_x = np.median(list_x)
+
+
+
                 plt.figure(figsize=(6,8))
                 plt.title(filename)
                 plt.xlim([0,width])
@@ -55,7 +63,12 @@ def graph():
                 plt.scatter(list_x,list_y)
                 plt.plot(list_x, y, color='black')
                 plt.text(0.1,a*0.1+b, 'y='+ str(round(a,4)) +'x+'+str(round(b,4)) + '(' + str(d2_average_sqrt) + ')')
+                plt.vlines(x=center, ymin=0, ymax=height, colors='r', linewidths=1)
+                plt.vlines(x=median_x, ymin=0, ymax=height, colors='g', linewidths=1)
                 plt.savefig(outputpass)
+
+                print(filename)
+print('オワオワリで～す')
 
 
 if __name__ == '__main__':
--- jsonforcsv.py
+++ jsonforcsv.py
@@ -1,51 +1,85 @@
 import json
 from statistics import mean, median,variance,stdev
 import numpy as np
+import math
 
 kigou = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-with open('./images/images_testing/output/output1.json','r') as input:
-  with open('./images/images_testing/output/output1.csv','a') as output:
-    json_dict = json.load(input)
 
-    output.write('filename,number,A,B,mean_x,mean_y,median_x,median_y,variance_x,variance_y,stdev_x,stdev_y\n')
+def main():    
+  with open('./images/images_testing2/output/output1.json','r') as input:
+    with open('./images/images_testing2/output/output1.csv','a') as output:
+      json_dict = json.load(input)
 
-    for item in json_dict:
-      count = item["num_of_points"]
-      width = item["width"]
-      height = item["height"]
-      list_x = np.array([])
-      list_y = np.array([])
-      for i in range(count):
-        list_x = np.append(list_x,item[kigou[i]]["x"])
-        list_y = np.append(list_y, height - item[kigou[i]]["y"])
+      output.write('filename,number,A,B,mean_x,mean_y,median_x,median_y,variance_x,\variance_y,stdev_x,stdev_y,range_x,center_x,range_y,center_y,ave_distance\n')
 
-        num_of_points = len(list_x)
+      for item in json_dict:
+        count = item["num_of_points"]
+        width = item["width"]
+        height = item["height"]
+        list_x = np.array([])
+        list_y = np.array([])
+        for i in range(count):
+          list_x = np.append(list_x,item[kigou[i]]["x"])
+          list_y = np.append(list_y, height - item[kigou[i]]["y"])
 
-        a, b = np.polyfit(list_x,list_y,1) 
+          num_of_points = len(list_x)
 
-        mean_x = np.mean(list_x)
-        mean_y = np.mean(list_y)
-        median_x = np.median(list_x)
-        median_y = np.median(list_y)
-        variance_x = np.var(list_x)
-        variance_y = np.var(list_y)
-        stdev_x = np.std(list_x)
-        stdev_y = np.std(list_y)
+          a, b = np.polyfit(list_x,list_y,1) 
 
-        output_string = item['filename'] +','+ \
-                        str(num_of_points) +','+ \
-                        str(a) +','+ \
-                        str(b) +','+ \
-                        '{0:.2f}'.format(mean_x) +','+ \
-                        '{0:.2f}'.format(mean_y) +','+ \
-                        '{0:.2f}'.format(median_x) +','+ \
-                        '{0:.2f}'.format(median_y) +','+ \
-                        '{0:.2f}'.format(variance_x) +','+ \
-                        '{0:.2f}'.format(variance_y) +','+ \
-                        '{0:.2f}'.format(stdev_x) +','+ \
-                        '{0:.2f}'.format(stdev_y) +','+ \
-                        '\n'
-      output.write(output_string)
+          mean_x = np.mean(list_x)
+          mean_y = np.mean(list_y)
+          median_x = np.median(list_x)
+          median_y = np.median(list_y)
+          variance_x = np.var(list_x)
+          variance_y = np.var(list_y)
+          stdev_x = np.std(list_x)
+          stdev_y = np.std(list_y)
 
-print('オワオワリで～す')
+          min_x = min(list_x)
+          max_x = max(list_x)
+          range_x = abs(max_x - min_x)
+          center_x = int(min_x + (range_x/2))
+
+          min_y = min(list_y)
+          max_y = max(list_y)
+          range_y = abs(max_y - min_y)
+          center_y = int(min_y + (range_y/2))
+
+          list_distance = []
+          for j in range(len(list_x)):
+            d = distance(a, b, list_x[j], list_y[j])
+            list_distance = np.append(list_distance, d)
+          ave_distance = int(np.mean(list_distance))
+
+
+          output_string = item['filename'] +','+ \
+                          str(num_of_points) +','+ \
+                          '{0:.2f}'.format(a) +','+ \
+                          '{0:.2f}'.format(b) +','+ \
+                          '{0:.2f}'.format(mean_x) +','+ \
+                          '{0:.2f}'.format(mean_y) +','+ \
+                          '{0:.2f}'.format(median_x) +','+ \
+                          '{0:.2f}'.format(median_y) +','+ \
+                          '{0:.2f}'.format(variance_x) +','+ \
+                          '{0:.2f}'.format(variance_y) +','+ \
+                          '{0:.2f}'.format(stdev_x) +','+ \
+                          '{0:.2f}'.format(stdev_y) +','+ \
+                          str(range_x)+','+ \
+                          str(center_x) +','+ \
+                          str(range_y) +','+ \
+                          str(center_y) +','+ \
+                          str(ave_distance) + '\n'
+
+        output.write(output_string)
+
+      print('オワオワリで～す')
+
+
+def distance(a,b,x1,y1):
+  d = abs(a*x1-y1+b)/math.sqrt(a**2 + 1)
+  return d
+
+
+if __name__ == '__main__':
+    main()
